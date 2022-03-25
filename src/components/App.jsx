5e709d34734fb1285e6cc47f7703e6f0bc79f400
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-03-25 23:15:09 +0200

    Зроблено Крок3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,41 @@ import { nanoid } from 'nanoid';
 
 export default class App extends Component {
   state = {
-    contacts: [],
+    // contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
+    number: '',
+    filter: '',
   };
-  handleNameChange = event => {
-    this.setState({ name: event.currentTarget.value });
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+    // this.setState(prevState => {
+    //   prevState.contacts = prevState.contacts.filter(e =>
+    //     e.name.includes({ stateFilter })
+    //   );
+    // });
   };
+
   handleOnSubmit = e => {
     e.preventDefault();
-    // this.setState({ contacts.push({ name: this.state.name, id: nanoid() }) });
-    //   ;
-    // });
     this.setState(prevState => {
-      prevState.contacts.push({ name: this.state.name, id: nanoid() });
+      prevState.contacts.push({
+        id: nanoid(),
+        name: this.state.name,
+        number: this.state.number,
+      });
     });
-    // console.log(...this.prevState.contacts);
-    //   [...this.prevState.contacts, { name: this.state.name, id: nanoid() }]
     this.reset();
   };
 
   reset = () => {
-    this.setState({ name: '' });
+    this.setState({ name: '', number: '' });
   };
 
   render() {
@@ -39,17 +53,48 @@ export default class App extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
-              onChange={this.handleNameChange}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            <p></p>
+            <p>Number </p>
+            <input
+              type="tel"
+              name="number"
+              onChange={this.handleChange}
+              value={this.state.number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
             />
           </label>
           <p></p>
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+
+        <label>
+          <p>Find contact by name</p>
+          <input
+            type="input"
+            name="filter"
+            onChange={this.handleChange}
+            value={this.state.filter}
+          />
+        </label>
         <ul>
-          {this.state.contacts.map(contact => {
-            return <li key={contact.id}>{contact.name}</li>;
-          })}
+          {this.state.contacts
+            .filter(e =>
+              e.name.toLowerCase().includes(this.state.filter.toLowerCase())
+            )
+            .map(contact => {
+              return (
+                <li key={contact.id}>
+                  {contact.name}: {contact.number}
+                </li>
+              );
+            })}
         </ul>
       </>
 
